d2be3b7ba60cad42784afada63339d6d74f45673
vobbla16 <novikovprojects@gmail.com>
2023-10-30 00:10:03 +0300

    allow insure pkg & update system

--- home-manager/shared/default.nix
+++ home-manager/shared/default.nix
@@ -52,6 +52,10 @@
       allowUnfree = true;
       # Workaround for https://github.com/nix-community/home-manager/issues/2942
       allowUnfreePredicate = (_: true);
+
+      permittedInsecurePackages = [
+        "electron-24.8.6"
+      ];
     };
   };
 
