f664459a1ec0e8a974026a55d521703367186a2f
vobbla16 <novikovprojects@gmail.com>
2024-01-15 23:56:24 +0300

    disable shadows; new animations curve

--- home-manager/main-pc/home.nix
+++ home-manager/main-pc/home.nix
@@ -16,17 +16,13 @@
         animations {
           enabled = true
 
-          bezier = overshot, 0.05, 0.9, 0.1, 1.05
-          bezier = smoothOut, 0.36, 0, 0.66, -0.56
-          bezier = smoothIn, 0.25, 1, 0.5, 1
+          bezier = ease,0.4,0.02,0.21,1
 
-          animation = windows, 1, 5, overshot, slide
-          animation = windowsOut, 1, 4, smoothOut, slide
-          animation = windowsMove, 1, 4, default
-          animation = border, 1, 5, default
-          animation = fade, 1, 5, smoothIn
-          animation = fadeDim, 1, 5, smoothIn
-          animation = workspaces, 1, 6, default
+          animation = windows, 1, 3.5, ease, slide
+          animation = windowsOut, 1, 3.5, ease, slide
+          animation = border, 1, 6, default
+          animation = fade, 1, 3, ease
+          animation = workspaces, 1, 3.5, ease
         }
 
         decoration {
@@ -39,10 +35,7 @@
             new_optimizations = on
           }
 
-          drop_shadow = yes
-          shadow_range = 4
-          shadow_render_power = 3
-          col.shadow = rgba(717c7cee)
+          drop_shadow = no
         }
     '' + (import ../shared/hyprland-config.nix);
   };
